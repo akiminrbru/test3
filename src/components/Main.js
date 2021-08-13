096e53a7440292fa936a85b4d74e864dbac17542
kiskisnotdead <akimin97@mail.ru>
2021-08-13 04:02:58 +0300

    Еле как сделал все(массивы)

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,34 +4,35 @@ import Cart from './Cart';
 import axios from 'axios';
 
 
-
 function Main() {
 
-    const [moviesData, setMoviesData] = useState( {
-        movies: undefined
+    const [moviesData, setMoviesData] = useState({
+        picture: [],
+        title: []
     });
 
     axios.get("https://yts.mx/api/v2/list_movies.json")
-        .then(response => response.data.data.movies[0].medium_cover_image)
-        .then(data => setMoviesData({
-            ...moviesData,
-            movies: data
-        }));
+    .then(response => response.data.data.movies)
+    .then(data => setMoviesData({
+        ...moviesData,
+        picture: data.map(x => x.medium_cover_image),
+        title: data.map(x => x.title)
+    }))
 
     return(
         <div>
             <Header/>
             <div className="main">
-                <Cart id={"1"} picture={moviesData.movies}/>
-                <Cart id={"2"}/>
-                <Cart id={"3"}/>
-                <Cart id={"4"}/>
+                <Cart id={"1"} picture={moviesData.picture[0]}/>
+                <Cart id={"2"} picture={moviesData.picture[1]}/>
+                <Cart id={"3"} picture={moviesData.picture[2]}/>
+                <Cart id={"4"} picture={moviesData.picture[3]}/>
             </div>
             <div className="main">
-                <Cart id={"5"}/>
-                <Cart id={"6"}/>
-                <Cart id={"7"}/>
-                <Cart id={"8"}/>
+                <Cart id={"5"} picture={moviesData.picture[4]}/>
+                <Cart id={"6"} picture={moviesData.picture[5]}/>
+                <Cart id={"7"} picture={moviesData.picture[6]}/>
+                <Cart id={"8"} picture={moviesData.picture[7]}/>
             </div>
         </div>
     )
