f5b70ecfa8f09691675e9a8fef5a752a02bc4301
kiskisnotdead <akimin97@mail.ru>
2021-08-13 20:13:45 +0300

    Немного изменений

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,31 +3,47 @@ import React from 'react';
 
 function Cart(props) {
 
-    const aStyle = {
+    const displayNone = {
         display: 'none'
     };
 
     function enter() {
         document.getElementById(props.id).style.display = "block";
-        document.getElementById(props.id).style.width = "58px";
-        document.getElementById(props.id).style.height = "29px";
+        document.getElementById(props.id).style.width = "119px";
+        document.getElementById(props.id).style.height = "39px";
+        document.getElementById(props.id).style.background = "#299DED";
         document.getElementById(props.id).style.position = "absolute";
-        document.getElementById(props.id).style.fontSize= "25px";
-        document.getElementById(props.id).style.marginLeft = "95px";
+        document.getElementById(props.id).style.borderRadius = "5px";
+        document.getElementById(props.id).style.marginLeft = "64px";
         document.getElementById(props.id).style.marginTop = "280px";
-        document.getElementById(props.id).style.fontSize= "25px";
-        document.getElementById(props.id).style.color= "#303539";
-        document.getElementById(props.id).style.textDecoration= "none";
+        document.getElementById(props.id2).style.fontSize = "25px";
+        document.getElementById(props.id2).style.color = "#303539";
+        document.getElementById(props.id2).style.fontWeight = "bold";
+        document.getElementById(props.id2).style.marginLeft = "30px";
+        document.getElementById(props.id2).style.lineHeight = "150%";
+        document.getElementById(props.id2).style.textDecoration= "none";
+        document.getElementById(props.id3).style.border= "5px solid #299DED";
+        document.getElementById(props.id4).style.display = "block";
+        document.getElementById(props.id4).style.position = "absolute";
+        document.getElementById(props.id4).style.fontWeight = "bold";
+        document.getElementById(props.id4).style.marginLeft = "110px";
     }
     
     function leave() {
         document.getElementById(props.id).style.display = "none";
+        document.getElementById(props.id3).style.border= "5px solid #FFFFFF";
+        document.getElementById(props.id4).style.display = "none";
     }
 
     return(
         <div onPointerLeave={leave} onPointerEnter={enter} className="image">
-            <a id={props.id} style={aStyle} href="#">More</a>
-            <img src={props.picture}/>
+            <p style={displayNone} id={props.id4} >{props.rating}</p>
+            <div id={props.id} style={displayNone}>
+                <a id={props.id2} href="#">More</a>
+            </div>
+            <div>
+                <img id={props.id3} src={props.picture}/>
+            </div>
         </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,8 @@ function Main() {
 
     const [moviesData, setMoviesData] = useState({
         picture: [],
-        title: []
+        title: [],
+        rating: [],
     });
 
     axios.get("https://yts.mx/api/v2/list_movies.json")
@@ -16,23 +17,24 @@ function Main() {
     .then(data => setMoviesData({
         ...moviesData,
         picture: data.map(x => x.medium_cover_image),
-        title: data.map(x => x.title)
+        title: data.map(x => x.title),
+        rating: data.map(x => x.rating),
     }))
 
     return(
         <div>
             <Header/>
             <div className="main">
-                <Cart id={"1"} picture={moviesData.picture[0]}/>
-                <Cart id={"2"} picture={moviesData.picture[1]}/>
-                <Cart id={"3"} picture={moviesData.picture[2]}/>
-                <Cart id={"4"} picture={moviesData.picture[3]}/>
+                <Cart id={"1"} id2={"a1"} id3={"p1"} id4={"t1"} picture={moviesData.picture[0]} rating={moviesData.rating[0]}/>
+                <Cart id={"2"} id2={"a2"} id3={"p2"} id4={"t2"} picture={moviesData.picture[1]} rating={moviesData.rating[1]}/>
+                <Cart id={"3"} id2={"a3"} id3={"p3"} id4={"t3"} picture={moviesData.picture[2]} rating={moviesData.rating[2]}/>
+                <Cart id={"4"} id2={"a4"} id3={"p4"} id4={"t4"} picture={moviesData.picture[3]} rating={moviesData.rating[3]}/>
             </div>
             <div className="main">
-                <Cart id={"5"} picture={moviesData.picture[4]}/>
-                <Cart id={"6"} picture={moviesData.picture[5]}/>
-                <Cart id={"7"} picture={moviesData.picture[6]}/>
-                <Cart id={"8"} picture={moviesData.picture[7]}/>
+                <Cart id={"5"} id2={"a5"} id3={"p5"} id4={"t5"} picture={moviesData.picture[4]} rating={moviesData.rating[4]}/>
+                <Cart id={"6"} id2={"a6"} id3={"p6"} id4={"t6"} picture={moviesData.picture[5]} rating={moviesData.rating[5]}/>
+                <Cart id={"7"} id2={"a7"} id3={"p7"} id4={"t7"} picture={moviesData.picture[6]} rating={moviesData.rating[6]}/>
+                <Cart id={"8"} id2={"a8"} id3={"p8"} id4={"t8"} picture={moviesData.picture[7]} rating={moviesData.rating[7]}/>
             </div>
         </div>
     )
